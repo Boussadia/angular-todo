6e5af08ade80f269402308dcc41e6480a84cd404
Ahmed Boussadia <ahmed.boussadia@gmail.com>
2013-11-08 11:36:25 +0100

    Adding local storage to appluication (and comments)

--- app/js/app.js
+++ app/js/app.js
@@ -1,18 +1,21 @@
 'use strict';
 
+// Initialisation d'une application Angular avec les dépendances dont nous avons besoin
 var TodoListApp = angular.module('TodoListApp', [
 	'ngRoute',
 	'TodoControllers',
-	'todoListFilter'
+	'todoListFilter',
+	'LocalStorageModule'
 ]);
 
 
+// Le routeur qui va nous permettre d'éxécuter des controller en fonction de l'url du hash
 TodoListApp.config(['$routeProvider',
 	function($routeProvider){
 		$routeProvider
 			.when('/todos', { templateUrl: 'partials/main.html', controller: 'MainCtrl'})
-			.when('/todos/new', {templateUrl: 'partials/new.html', controller: 'NewTodoCtrl'})
-			.when('/todos/:todoId', {templateUrl: 'partials/new.html', controller: 'EditTodoCtrl'})
+			.when('/todos/new', {templateUrl: 'partials/todo.html', controller: 'NewTodoCtrl'})
+			.when('/todos/:todoId', {templateUrl: 'partials/todo.html', controller: 'EditTodoCtrl'})
 			.otherwise({
 				redirectTo: '/todos'
 			});
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,18 +1,48 @@
 'use strict';
 
+
+// Instanciation de notre module qui contient tous les controllers
 var TodoControllers = angular.module('TodoControllers', []);
 
+
+// Variable globale qui contient nos taches
 var todos = [];
 
-TodoControllers.controller('MainCtrl', ['$scope',
-	function MainCtrl($scope){
-		$scope.todos = todos;
 
+// Premier controller qui s'occupe de gérer la page principale
+TodoControllers.controller('MainCtrl', ['$scope', 'localStorageService',
+	function MainCtrl($scope,localStorageService){
+		$scope.todos = todos;
+		var nb_tasks = localStorageService.get('nb_tasks') || "0";
+		nb_tasks = parseInt(nb_tasks);
+		
+		if(nb_tasks !== todos.length){
+
+			for(var i = 0; i < nb_tasks; i++){
+				var task = JSON.parse(localStorageService.get('task_'+i));
+				todos.push(task);
+			}
+		}
+		
+		localStorageService.add('nb_tasks', nb_tasks);
+
+
+		$scope.setDone = function(index){
+			var todo = todos[index];
+			todo = {
+				'title': todo.title,
+				'description': todo.description,
+				'done': !todo.done
+			};
+			localStorageService.add('task_'+index, JSON.stringify(todo));
+		}
 	}
 ]);
 
-TodoControllers.controller('EditTodoCtrl', ['$scope', '$routeParams', '$location',
-	function EditTodoCtrl($scope, $routeParams, $location){
+
+// Controller qui permet de gérer l'édition d'une tache
+TodoControllers.controller('EditTodoCtrl', ['$scope', '$routeParams', '$location', 'localStorageService',
+	function EditTodoCtrl($scope, $routeParams, $location,localStorageService){
 		var todoId = $routeParams.todoId;
 		$scope.state = "edit";
 
@@ -30,27 +60,33 @@ TodoControllers.controller('EditTodoCtrl', ['$scope', '$routeParams', '$location
 			if(title){
 				todos[todoId] = {
 					'description': description,
-					'title': title
+					'title': title,
+					'done': todos[todoId].done
 				}
+				localStorageService.add('task_'+todoId, JSON.stringify(todos[todoId]));
 				$location.path('/');
 			}else{
 				alert("Le titre ne doit pas être vide !");
 			}
 		}
 
-
 	}
 ]);
 
-TodoControllers.controller('NewTodoCtrl', ['$scope', '$location', 
-	function NewTodoCtrl($scope, $location){
+
+// Controller qui permet de créer une nouvelle tache
+TodoControllers.controller('NewTodoCtrl', ['$scope', '$location',  'localStorageService',
+	function NewTodoCtrl($scope, $location,localStorageService){
 		$scope.title = "";
 		$scope.description = "";
 		$scope.state = "new";
 
 		$scope.addNewTodo = function(title, description){
 			if(title){
-				todos.push({'title': title, "description": (description || "")})
+				todos.push({'title': title, "description": (description || ""), done: false});
+				var l = todos.length;
+				localStorageService.add('task_'+(l-1), JSON.stringify(todos[l-1]));
+				localStorageService.add('nb_tasks', l);
 				$location.path('/');
 			}else{
 				alert("Le titre ne doit pas être vide !");
